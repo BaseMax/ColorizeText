23e15f1120fe0bf4167da094ff8c3fb05b43f802
Max Base <MaxBaseCode@Gmail.Com>
2019-06-17 19:32:42 +0430

    Update system.c

--- system.c
+++ system.c
@@ -1,8 +1,30 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
+/*
+Color List:
+
+	0 = Black
+	1 = Blue
+	2 = Green
+	3 = Aqua
+	4 = Red
+	5 = Purple
+	6 = Yellow
+	7 = White
+	8 = Gray
+	9 = Light Blue
+	A = Light Green
+	B = Light Aqua
+	C = Light Red
+	D = Light Purple
+	E = Light Yellow
+	F = Bright White
+*/
+
 int main() {
 	system("color 1");
+	// system("Color F0");
 	// system("color 41")
 	printf("This is a sample text!\n");
 	return 0;
