a483b84763c731bb8433d2f3c4d82b1ddc6151cf
Max Base <MaxBaseCode@Gmail.Com>
2019-06-17 19:38:40 +0430

    Update rlutil.c

--- rlutil.c
+++ rlutil.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "rlutil.h" // http://tapiov.net/rlutil/
+#include "rlutil.h" // http://tapiov.net/rlutil/, http://tapiov.net/rlutil/docs/HTML/files/docs/APIReference-txt.html
 
 int main(int argc, char* argv[]) {
 	setColor(BLUE);
