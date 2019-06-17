89f5af85d543dbfc78c4e40157fec02cac05444e
Max Base <MaxBaseCode@Gmail.Com>
2019-06-17 19:25:59 +0430

    Create function.c

---
+++ function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void red () {
+	printf("\033[1;31m");
+}
+
+void yellow {
+	printf("\033[1;33m");
+}
+
+void reset () {
+	printf("\033[0m");
+}
+
+int main () {
+	red();
+	printf("Hello, ");
+	yellow();
+	printf("World!\n");
+	reset;
+	return 0;
+}
