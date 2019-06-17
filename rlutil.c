039d9f66d42a910367b6e060df13d9a637cf847f
Max Base <MaxBaseCode@Gmail.Com>
2019-06-17 19:38:52 +0430

    Update rlutil.c

--- rlutil.c
+++ rlutil.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include "rlutil.h" // http://tapiov.net/rlutil/, http://tapiov.net/rlutil/docs/HTML/files/docs/APIReference-txt.html
+#include "rlutil.h" // http://tapiov.net/rlutil/
 
 int main(int argc, char* argv[]) {
+	// http://tapiov.net/rlutil/docs/HTML/files/docs/APIReference-txt.html
 	setColor(BLUE);
 	printf("This is a sample text with custom color!\n");
 	getch();
